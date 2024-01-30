7a838df92b167726515b919e1ad77578773d82f1
RiccardoImperato <riccardo.imperato@libero.it>
2024-01-30 13:06:34 +0100

    feat: stile base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,7 +16,7 @@
                 <li class="card" style="width: 18rem;">
                     <div class="card-body">
                         <h5 class="card-title">{{ $movie['title'] }}</h5>
-                        <h6 class="card-subtitle mb-2 text-body-secondary">{{ $movie['original_title'] }}</h6>
+                        <h6 class="card-subtitle my-2">{{ $movie['original_title'] }}</h6>
                         <div class="card-text">{{ $movie['nationality'] }}</div>
                         <div class="card-text">{{ $movie['date'] }}</div>
                         <div class="card-text">{{ $movie['vote'] }}</div>
